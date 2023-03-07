6fcbb6c70d5508debf7a5d34937d6b604dd9e683
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-06 19:22:31 -0700

    added role table to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,10 +5,20 @@ ZEROFILL -Specifies the ID should be 0 padded to the left
 AUTO_INCREMENT -IS used in conjuction w/ primary key, incrementing each id to ensure that each ID is unique
 */
 
-CREATE TABLE departments (
+CREATE TABLE department (
   id INT(5) UNSIGNED ZEROFILL AUTO_INCREMENT PRIMARY KEY
   name VARCHAR(30) NOT NULL
 );
 
+/*
+decimal(X,Y) - x denotes numbers to the left of the decimal, y denotes numbers to the right
+FOREIGN KEY - A constraint that ensures data integrity via creating an id link between the key & reference
+*/
 
-
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES departments(id)
+);
