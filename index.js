96c66c4429373e3cfcaa72dea0803c805ba2d223
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-06 17:19:31 -0700

    began work on startProgram constant

--- index.js
+++ index.js
@@ -9,4 +9,26 @@ const connection = mysql.createConnection({
     user: 'root',
     password: '',
     database: 'employee_tracker'
-  });
+});
+
+
+// Start Program Function to return various options
+const startProgram = () => {
+    return inquirer.prompt ({
+        type: "list",
+        name: "start-menu",
+        message: "Select an option:",
+        choices: [
+            "View Deparments",
+            "View Roles",
+            "View Employees",
+            "Add Department",
+            "Add Role",
+            "Add Employee",
+            "Update Eployee Role",
+            "Quit"
+        ]
+    })
+}
+
+
