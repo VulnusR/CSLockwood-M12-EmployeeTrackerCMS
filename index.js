b0657bb7bddcbf1ad4a0919fc483561bd85fbb12
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-06 17:04:37 -0700

    established connection to DB

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+
+
+//Establishes MySql DB Connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_tracker'
+  });
